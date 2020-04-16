6c461d9c73e683baf6318d895da69621f3bc0659
mlac <lachheb@brocelia.fr>
2020-04-16 17:30:57 +0200

    Handling bottom app bar elements:  * add contextual menu with icons  * handle list and grid view button icon change  * add nav drawer with menu and icons  * handle fab click

--- app/src/main/java/com/example/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/example/shoppinglist/MainActivity.kt
@@ -1,12 +1,75 @@
 package com.example.shoppinglist
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.google.android.material.bottomappbar.BottomAppBar
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var coordinatorLayout: CoordinatorLayout
+    private lateinit var bar: BottomAppBar
+    private lateinit var fab : FloatingActionButton
+    private lateinit var menu : Menu
+
+    private var isListViewType = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        coordinatorLayout = coordinator_layout
+        bar = bottom_app_bar
+        fab = floating_action_button
+        setSupportActionBar(bar)
+        fab.setOnClickListener {
+            showSnackbar("FAAAAAAAAAAAAB !")
+        }
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottomappbar_menu, menu)
+        this.menu = menu
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+            R.id.app_bar_cards -> showSnackbar("Fav menu item is clicked!")
+            R.id.app_bar_view_type -> handleViewType()
+            android.R.id.home -> {
+                val bottomNavDrawerFragment = BottomNavigationDrawerFragment()
+                bottomNavDrawerFragment.show(supportFragmentManager, bottomNavDrawerFragment.tag)
+            }
+        }
+
+        return true
+    }
+
+    private fun showSnackbar(text: CharSequence) {
+        val anchorView : View = if (fab.visibility == View.VISIBLE) fab else bar
+        Snackbar.make(coordinatorLayout, text, Snackbar.LENGTH_SHORT)
+            .setAnchorView(anchorView)
+            .show()
+    }
+
+    private fun handleViewType(){
+        if (isListViewType){
+            isListViewType = false
+            showSnackbar("Grid view mode")
+            menu.getItem(1).icon = resources.getDrawable(R.drawable.ic_view_list_black_24dp, this.theme)
+
+        } else {
+            isListViewType = true
+            showSnackbar("List view mode")
+            menu.getItem(1).icon = resources.getDrawable(R.drawable.ic_view_module_black_24dp, this.theme)
+        }
     }
 }
